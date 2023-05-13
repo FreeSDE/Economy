fe8a952a0a98ea3f24e10590337cf75dd63b7d50
Rayiscool1234 <rayan.almoabadi@gmail.com>
2023-05-13 16:26:37 +0300

    Update 1

--- Economy/Economy.cpp
+++ Economy/Economy.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <limits>
 #include <vector>
+#include <map>
+#include <unordered_map>
 void clear_screen() {
 #ifdef _WIN32
     system("CLS");
@@ -9,9 +11,24 @@ void clear_screen() {
     system("clear");
 #endif
 }
+void wait_for_enter() {
+#ifdef _WIN32
+    system("pause");
+#else
+    system("clear");
+#endif
+}
 std::string List = "1. Buy products\n2. Sell products\n3. Option\n4. Help\n5. Exit";
 std::string Version = "V0.1 Economy Sim";
 bool Devmode;
+
+
+// Map PlayerID to their money
+std::map<int, double> playerMoney;
+
+// Map PlayerID to their inventory, which is another map of ItemID to the amount of that item they own
+std::map<int, std::unordered_map<int, int>> playerInventory;
+
 /*
 Values needed to be found before doing the formula
     
@@ -22,7 +39,7 @@ Formula for prices & How to run this
 
     P = (D / (S - Cₚ)) * (IBᵥ)
 
-    { Demand Divided by Supply times by constant of 10 Equals the Price }
+    { Supply minus Production cost which is than the demand gets divided by the new supply before multiplying, Inflation Times Base Value which than, we multiple the two numbers founded which equals the price }
 
     IF no Supply than
     - Product is unavailable
@@ -36,22 +53,52 @@ Formula for prices & How to run this
     - Demand goes back to normal
     - Supply goes back to normal
 */
+
 class Items {
 public:
     std::string Name;
     double Supply;
     double Demand;
     int ItemID;
+    double Price;
+    double Bv;  // Initial base value
+    double ProductionCost;  // The cost to produce the item
+    double Inflation;  // The rate of inflation
+
     static std::vector<Items*>& getAllItems() {
         static std::vector<Items*> allItems;
         return allItems;
     }
-    Items(double Asupply, double Ademand, std::string Aname, int AitemID)
-        : Name(Aname), Supply(Asupply), Demand(Ademand), ItemID(AitemID) {
-        getAllItems().push_back(this);
-    }
     int getItemID() const {
         return ItemID;
+    }
+
+    Items(double Asupply, double Ademand, std::string Aname, int AitemID, double AproductionCost, double Ainflation)
+        : Name(Aname), Supply(Asupply), Demand(Ademand), ItemID(AitemID), ProductionCost(AproductionCost), Inflation(Ainflation) {
+        Bv = Demand / Supply;  // Calculate the initial base value
+        Price = calculatePrice();
+        getAllItems().push_back(this);
+    }
+
+    void setSupply(double newSupply) {
+        Supply = newSupply;
+        valueChanged();
+    }
+
+    void setDemand(double newDemand) {
+        Demand = newDemand;
+        valueChanged();
+    }
+
+private:
+    void valueChanged() {
+        Price = calculatePrice();
+    }
+
+    double calculatePrice() {
+        double adjustedSupply = Supply - ProductionCost;
+        double demandOverSupply = Demand / adjustedSupply;
+        return demandOverSupply * (Inflation * Bv);
     }
 };
 
@@ -74,20 +121,84 @@ bool Buy(int ItemID) {
 bool Sell(int ItemID) {
     return false;
 }
+std::string EconomyHandler(int PlayerID, int ItemID, int Amount, std::string Action) {
+    std::string Output;
+    if (PlayerID == 0) {
+        Devmode = true;
+    }
+
+    std::vector<Items*>& allItems = Items::getAllItems();
+    for (auto& item : allItems) {
+        if (item->ItemID == ItemID) {
+            if (Action == "Buy") {
+                if (item->Supply < Amount) {
+                    Output = "Insufficient supply to buy.";
+                    wait_for_enter();
+                }
+                else {
+                    double cost = item->Price * Amount;
+                    if (playerMoney[PlayerID] < cost) {
+                        Output = "Insufficient funds to buy.";
+                        wait_for_enter();
+                    }
+                    else {
+                        playerMoney[PlayerID] -= cost;
+                        item->setSupply(item->Supply - Amount);
+                        playerInventory[PlayerID][ItemID] += Amount;
+                        Output = "Successfully bought " + std::to_string(Amount) + " " + item->Name;
+                        wait_for_enter();
+                    }
+                }
+            }
+            else if (Action == "Sell") {
+                if (playerInventory[PlayerID][ItemID] < Amount) {
+                    Output = "Insufficient items to sell.";
+                    wait_for_enter();
+                }
+                else {
+                    playerMoney[PlayerID] += item->Price * Amount;
+                    item->setSupply(item->Supply + Amount);
+                    playerInventory[PlayerID][ItemID] -= Amount;
+                    Output = "Successfully sold " + std::to_string(Amount) + " " + item->Name;
+                    wait_for_enter();
+                }
+            }
+            else {
+                Output = "Invalid action.";
+                wait_for_enter();
+            }
+            break;
+        }
+    }
+
+    return Output;
+}
 
 void Economy(int Option) {
+    int ItemID, Amount;
+    std::string Action;
     switch (Option) {
     case 1:
-        // Add implementation for option 1
+        std::cout << "Enter the ID of the item you want to buy: ";
+        std::cin >> ItemID;
+        std::cout << "Enter the amount you want to buy: ";
+        std::cin >> Amount;
+        Action = "Buy";
+        std::cout << EconomyHandler(0, ItemID, Amount, Action) << std::endl;
         break;
     case 2:
-        // Add implementation for option 2
+        std::cout << "Enter the ID of the item you want to sell: ";
+        std::cin >> ItemID;
+        std::cout << "Enter the amount you want to sell: ";
+        std::cin >> Amount;
+        Action = "Sell";
+        std::cout << EconomyHandler(0, ItemID, Amount, Action) << std::endl;
         break;
     case 3:
-        // Add implementation for option 3
+
         break;
     case 4:
-        // Add implementation for option 4
+
         break;
     default:
         std::cout << "You inputted an option that didn't exist" << std::endl;
@@ -95,8 +206,9 @@ void Economy(int Option) {
     }
 }
 
+
 void initializeEconomy() {
-    Items Item1(1.0, 1.0, "Planks", 1);
+    Items Item1(1.0, 1.0, "Planks", 1, 0.5, 1);
 }
 
 void displayMenu() {
@@ -162,16 +274,8 @@ void EconomyManager() {
     } while (!done || done);
 }
 
-std::string EconomyHandler(int PlayerID, int ItemID, int Amount) {
-    std::string Output;
-    if (PlayerID == 0) {
-        Devmode = true;
-    }
-    
-     = Items::getAllItems();
-    
-    return Output;
-}
+
+
 
 void EconomyMain() {
     initializeEconomy();
